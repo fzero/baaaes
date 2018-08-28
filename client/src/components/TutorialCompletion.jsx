ae2257aea1e3db0ed26e71d8757831fd1b512b62
Your Name <alex.zvaniga@gmail.com>
2018-08-28 16:24:36 -0400

    fixed exports

--- client/src/components/TutorialCompletion.jsx
+++ client/src/components/TutorialCompletion.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
-class CompletedTutorial extends Component {
+class TutorialCompletion extends Component {
   render() {
-    return(
+    return (
       <section>
         <h3>You Have Completed the Tutorial!</h3>
-        <p>Congratulations on completing the blockchain tutorial. You are now knowledgable enough
-        to send and recieve cryptocurrency for real!</p>
+        <p>
+          Congratulations on completing the blockchain tutorial. You are now
+          knowledgable enough to send and recieve cryptocurrency for real!
+        </p>
         <button>Restart Tutorial</button>
         <button>Finish Tutorial</button>
       </section>
-    )
+    );
   }
 }
+export default TutorialCompletion;
