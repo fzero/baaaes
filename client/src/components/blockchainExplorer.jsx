31b4dfb678592f821b1059af9fc8cf6c8b77f5eb
Nikolas <njlatcham@gmail.com>
2018-08-28 19:55:52 +0000

    Finished adding tutorial components

---
+++ client/src/components/blockchainExplorer.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class BlockchainExplorer extends Component {
+  render() {
+    return(
+      <section>
+        <h3>Blockchain Transactions</h3>
+        <div>
+          <h5>Bought</h5>
+          <div>
+            <p>Cost:</p>
+            <p>Spent:</p>
+            <p>Sender Address</p>
+            <p>Recieving Address</p>
+          </div>
+        </div>
+
+        <div>
+          <h5>Bought</h5>
+          <div>
+            <p>Cost:</p>
+            <p>Spent:</p>
+            <p>Sender Address</p>
+            <p>Recieving Address</p>
+          </div>
+        </div>
+        <button>Finish</button>
+      </section>
+    )
+  }
+}
