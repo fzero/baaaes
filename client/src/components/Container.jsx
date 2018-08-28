7ca21328866d05ad74606ea31c74ed67b96cc5a7
Your Name <alex.zvaniga@gmail.com>
2018-08-28 16:22:20 -0400

    Finishing routes

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import Register from './Register.jsx';
-import SampleSell from './SampleSell.jsx';
-import Homepage from './homePage.jsx';
-import CreateMneumonic from './CreateMneumonic';
 import { Route, Switch } from 'react-router-dom';
+// Components
+import Homepage from './HomePage.jsx';
+import Register from './Register.jsx';
+import CreateMneumonic from './CreateMneumonic.jsx';
+import ConfirmMneumonic from './ConfirmMneumonic.jsx';
+import PublicKey from './PublicKey.jsx';
+import PrivateKey from './PrivateKey.jsx';
+import CreateWallet from './CreateWallet.jsx';
+import CoinInfo from './CoinInfo.jsx';
+import SampleSell from './SampleSell.jsx';
+import SampleBuy from './SampleBuy.jsx';
+import BlockchainExplorer from './BlockchainExplorer.jsx';
+import TutorialCompletion from './TutorialCompletion.jsx';
 
 const Container = props => (
   <div className="container">
     <Switch>
       <Route path="/" exact component={Homepage} />
-      <Route path="/samplesell" component={SampleSell} />
+      <Route path="/register" component={Register} />
       <Route path="/createmneumonic" component={CreateMneumonic} />
-      <Route path="/coininfo" component={Register} />
-      <Route path="/samplebuy" component={Register} />
-      <Route path="/blockchainexplorer" component={Register} />
-      <Route path="/confirmmneumonic" component={Register} />
-      <Route path="/createdwallet" component={Register} />
-      <Route path="/privatekey" component={Register} />
-      <Route path="/publickey" component={Register} />
-      <Route path="/tutorialcomplete" component={Register} />
+      <Route path="/confirmmneumonic" component={ConfirmMneumonic} />
+      <Route path="/publickey" component={PublicKey} />
+      <Route path="/privatekey" component={PrivateKey} />
+      <Route path="/createwallet" component={CreateWallet} />
+      <Route path="/coininfo" component={CoinInfo} />
+      <Route path="/samplesell" component={SampleSell} />
+      <Route path="/samplebuy" component={SampleBuy} />
+      <Route path="/blockchainexplorer" component={BlockchainExplorer} />
+      <Route path="/tutorialcomplete" component={TutorialCompletion} />
     </Switch>
   </div>
 );
