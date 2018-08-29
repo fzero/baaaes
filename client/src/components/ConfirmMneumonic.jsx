39c0acf82e69ebbeaffaa2734c56c2a21b19de10
Nikolas <njlatcham@gmail.com>
2018-08-29 16:11:14 +0000

    started adding descriptions

--- client/src/components/ConfirmMneumonic.jsx
+++ client/src/components/ConfirmMneumonic.jsx
@@ -6,14 +6,30 @@ class ConfirmMneumonic extends Component {
     return(
       <main className="componentContainer">
         <section className="description">
+          <h3>Mnemonic Phrase Description</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae,
-            error dolor consequuntur, natus eum itaque harum fugit perspiciatis
-            nobis id aperiam sed architecto et esse perferendis nisi dolorem
-            voluptas repellat.Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Beatae, error dolor consequuntur, natus eum itaque harum fugit
-            perspiciatis nobis id aperiam sed architecto et esse perferendis nisi
-            dolorem voluptas repellat.
+            A Mnemonic Phrase, also known as a "seed phrase" or "seed recovery phrase", 
+            is a randomly generated list of words which store all the information needed
+            to recover a Crypto-Wallet. In the event where a User's computer breaks, or
+            their hard-drive is corrupted, the Mnemonic Phrase can be used to recover the
+            wallet. Anyone who discovers your mnemonic phrase can use it to access your 
+            wallet and potentially steal everything within it.
+          </p>
+          <h4>How it works</h4>
+          <p>
+            Crypto-Wallet software contains a list of words from a dictionary, each
+            corresponding to an assigned number. The Mnemonic Phrase would be converted
+            into a number that then can be used to access the wallet. BIP39, the mnemonic phrase 
+            generator, contains 2048 words, and creates 12 words for the phrase. There
+            would be 2048^2 or 4,194,304 different possible combinations, which would 
+            be approximately 128 bits of security.
+          </p>
+          <p>Warning: Forgetting this Phrase could result in the loss of your wallet and 
+            everything it contains.
+          </p>
+          <p>
+            DO NOT overestimate your ability to memorize this phrase, please write it
+            down on a piece of paper instead, and store it in a secure location.
           </p>
         </section>
 
