720e14ce9bdd5a7703cf4d9c7b9a1ac028abe83f
Your Name <alex.zvaniga@gmail.com>
2018-08-29 16:30:20 -0400

    added data handlers from boilerplate

--- client/src/helpers/generator.js
+++ client/src/helpers/generator.js
@@ -1,5 +1,6 @@
 const bip39 = require('bip39');
 const HDKey = require('hdkey');
+const bip32 = require('bip32');
 
 var mnemonic = bip39.generateMnemonic();
 console.log(mnemonic);
@@ -7,5 +8,10 @@ console.log(mnemonic);
 const seed = bip39.mnemonicToSeedHex(mnemonic);
 console.log(`seed = ${seed}`);
 const hdkey = HDKey.fromMasterSeed(new Buffer(seed, 'hex'));
+const node = bip32.fromBase58(hdkey.privateExtendedKey);
+let child = node.derivePath('m/0/0');
+console.log(node);
+console.log(hdkey);
 console.log(`PRIVATE: ${hdkey.privateExtendedKey}`);
 console.log(`PUBLIC: ${hdkey.publicExtendedKey}`);
+console.log(`CHILD ${JSON.stringify(child)}`);
