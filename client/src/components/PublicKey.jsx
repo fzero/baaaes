153d10f72d05a11196560dac49805e9f00691138
Your Name <alex.zvaniga@gmail.com>
2018-08-29 16:36:00 -0400

    added publicKey

--- client/src/components/PublicKey.jsx
+++ client/src/components/PublicKey.jsx
@@ -13,21 +13,12 @@ class PublicKey extends Component {
         <section className="description">
           <h3>Public Key Description</h3>
           <p>
-<<<<<<< HEAD
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae,
-            error dolor consequuntur, natus eum itaque harum fugit perspiciatis
-            nobis id aperiam sed architecto et esse perferendis nisi dolorem
-            voluptas repellat.Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Beatae, error dolor consequuntur, natus eum itaque
-            harum fugit perspiciatis nobis id aperiam sed architecto et esse
-            perferendis nisi dolorem voluptas repellat.
-=======
-            A public key is derived from the private key, which will be discussed in 
-            the next step, using a complex mathematic algorithm, is used as the 
-            address where you will recieve transactions sent from someone else. It is 
-            a 256-bit number, and unlike a private key, a public key can be shared 
-            openly without any worry that someone will use it to access your wallet. 
->>>>>>> master
+            A public key is derived from the private key, which will be
+            discussed in the next step, using a complex mathematic algorithm, is
+            used as the address where you will recieve transactions sent from
+            someone else. It is a 256-bit number, and unlike a private key, a
+            public key can be shared openly without any worry that someone will
+            use it to access your wallet.
           </p>
         </section>
 
