2447a450cec33024ef83e547aad29b01835977d3
Your Name <alex.zvaniga@gmail.com>
2018-08-30 11:43:09 -0400

    helper

--- client/src/helpers/generator.js
+++ client/src/helpers/generator.js
@@ -1,18 +1,42 @@
 const bip39 = require('bip39');
 const HDKey = require('hdkey');
 const bip32 = require('bip32');
-var bitcoin = require('bitcoinjs-lib');
+const bitcoin = require('bitcoinjs-lib');
 
-var mnemonic = bip39.generateMnemonic();
-console.log(mnemonic);
+// GENERATES MNEMONIC
+const mnemonic = bip39.generateMnemonic();
 
-const seed = bip39.mnemonicToSeedHex(mnemonic);
-console.log(`seed = ${seed}`);
-const hdkey = HDKey.fromMasterSeed(new Buffer(seed, 'hex'));
-// Need to convert xpriv to private key
-// Testing various pieces of the object below.
-const private = bitcoin.ECPair.fromPrivateKey(hdkey.privateKey);
-console.log(`PRIVATE: ${hdkey.privateExtendedKey},
-short private: ${JSON.stringify(private)}`);
+console.log(
+  `MNEMONIC = ${mnemonic} \n\n----------------------------------------`
+);
 
-console.log(`PUBLIC: ${hdkey.publicExtendedKey}`);
+const seed = bip39.mnemonicToSeed(mnemonic);
+console.log(`SEED = ${seed}\n------------------------`);
+const node = bip32.fromSeed(seed);
+console.log(`WIF = ${node.toWIF()}`);
+console.log(`PUBLIC KEY= ${node.derive}`);
+// const string = node.toBase58();
+// console.log(string);
+// const restored = bip32.fromBase58(string);
+// console.log(restored);
+
+// // console.log(mnemonic);
+
+// const seed = bip39.mnemonicToSeedHex(mnemonic);
+// // console.log(`seed = ${seed}`);
+// const hdkey = HDKey.fromMasterSeed(new Buffer(seed, 'hex'));
+
+// key.compressed = false;
+// console.log(key.privateWif); // => 5Hx15HFGyep2CfPxsJKe2fXJsCVn5DEiyoeGGF6JZjGbTRnqfiD
+// // Need to convert xpriv to private key
+// // Testing various pieces of the object below.
+// const node = bip32.fromBase58(
+//   hdkey.privateExtendedKey,
+//   bitcoin.networks.mainnet
+// );
+// // console.log(`PRIVATE: ${hdkey.privateExtendedKey},
+// // short private: ${JSON.stringify(private)}`);
+
+// // console.log(`PUBLIC: ${hdkey.publicExtendedKey}`);
+// console.log(`PRIVATE: ${key.privateExtendedKey}`);
+// console.log(node.toWIF());
