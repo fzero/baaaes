9e5a8af399c02addf0f60330dc2fca4e3f60a3dd
Your Name <alex.zvaniga@gmail.com>
2018-09-02 18:59:02 -0400

    added type to keys in server models

--- client/src/helpers/balance.js
+++ client/src/helpers/balance.js
@@ -25,3 +25,29 @@ module.exports = function(address) {
       .catch(errors => reject(errors));
   });
 };
+
+{
+  /* <script type='text/javascript' id='coinbase_widget_loader' class='coinbase-widget-async-loader'> */
+}
+// document.currentScript.src = '';
+(function() {
+  function asyncLoad() {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    var theUrl = 'https://buy.coinbase.com/static/widget.js';
+    s.src =
+      theUrl +
+      (theUrl.indexOf('?') >= 0 ? '&' : '?') +
+      'ref=' +
+      encodeURIComponent(window.location.href);
+    var embedder = document.getElementById('coinbase_widget_loader');
+    embedder.parentNode.insertBefore(s, embedder);
+  }
+  if (window.attachEvent) {
+    window.attachEvent('onload', asyncLoad);
+  } else {
+    window.addEventListener('load', asyncLoad, false);
+  }
+})();
+// </script>
