5dae28c9604769bc565690fdaeaadfcdbdd04ba4
Your Name <alex.zvaniga@gmail.com>
2018-09-02 17:54:59 -0400

    address

--- client/src/helpers/balance.js
+++ client/src/helpers/balance.js
@@ -1,1 +1,27 @@
-const bitcoin = require('bitcoinjs-lib');
+const axios = require('axios');
+
+// This file will handle all market information
+
+const API_HOST = 'https://blockchain.info';
+const API_NAMESPACE = '/rawaddr';
+const BASEURL = `${API_HOST}${API_NAMESPACE}`;
+
+// Axios instance - Custom Headers go here
+const server = axios.create({
+  baseURL: BASEURL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// MARKET FUNCTIONS
+
+module.exports = function(address) {
+  return new Promise((resolve, reject) => {
+    server
+      .get(`/${address}`)
+      .then(result => resolve(result.data.RAW))
+      .catch(errors => reject(errors));
+  });
+};
