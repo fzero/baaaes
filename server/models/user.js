261d8faa898f46b8f915f010832c192c03112c67
Your Name <alex.zvaniga@gmail.com>
2018-09-03 19:36:10 -0400

    updated user default value

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,15 @@
-const Sequelize = require("sequelize");
-const uuid = require("uuid/v4");
+const Sequelize = require('sequelize');
+const uuid = require('uuid/v4');
 
 module.exports = sequelize => {
   const User = sequelize.define(
-    "users",
+    'users',
     {
       id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
-        defaultValue: uuid()
+        defaultValue: Sequelize.UUIDV1
       },
       username: {
         type: Sequelize.STRING,
